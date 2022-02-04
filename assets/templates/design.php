6af14361ecde3c0e5517dbd6b1c95f280b071f0d
Abdul Aziz Mubarak <abdulazizmubarak1@gmail.com>
2022-02-04 16:56:34 +0800

    delete sections service&portfolio

--- assets/templates/design.php
+++ assets/templates/design.php
@@ -1,2 +1,16 @@
-<link rel="stylesheet" href="boxicons.min.css">
-<link rel="stylesheet" href="bootstrap.min.css">
+<!-- Favicons -->
+<link rel="icon" href="../img/logoku.png">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+ <!-- Vendor CSS Styles  -->
+<link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="../vendor/bootstrap-icons/bootstrap-icons.css">
+<link rel="stylesheet" href="../vendor/boxicons/css/boxicons.min.css">
+<link rel="stylesheet" href="../vendor/glightbox/css/glightbox.min.css">
+<link rel="stylesheet" href="../vendor/remixicon/remixicon.css">
+<link rel="stylesheet" href="../vendor/swiper/swiper-bundle.min.css">
+
+<!-- Main CSS Style -->
+<link rel="stylesheet" href="../css/mystyle.css">
