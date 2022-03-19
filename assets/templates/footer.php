d41079191aab21279552ce90d094b9ebfb402f27
Abdul Aziz Mubarak <abdulazizmubarak1@gmail.com>
2022-03-19 17:11:00 +0800

    remove

--- assets/templates/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- Vendor JS Files -->
-<script src="../vendor/purecounter/purecounter.js"></script>
-<script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="../vendor/glightbox/js/glightbox.min.js"></script>
-<script src="../vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="../vendor/swiper/swiper-bundle.min.js"></script>
-<script src="../vendor/waypoints/noframework.waypoints.js"></script>
