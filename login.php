6c21940bfb49ac23aba9f1730318fcfeb8501ec3
Abdul Aziz Mubarak <32267976+mubarakabaz@users.noreply.github.com>
2022-04-04 21:22:26 +0800

    update login.php

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" rel="stylesheet">
   <!-- Bootstrap 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" >
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Login CSS -->
@@ -26,54 +26,22 @@
 
   <!-- form Login -->
   <div class="main">
-    <div class="container signup" id="signup">
-      <form action="#" method="POST" id="signup-form" class="form">
-        <h2 class="title-form title">Buat Akun Baru</h2>
-        <div class="icons-form">
-          <a href="https://www.facebook.com" class="icons-form"><i class="fab fa-facebook-f"></i></a>
-          <a href="https://www.instagram.com" class="icons-form"><i class="fab fa-instagram"></i></a>
-          <a href="https://www.linkedin.com" class="icons-form"><i class="fab fa-linkedin-in"></i></a>
-        </div>
-        <span class="span-form">silahkan masukkan data anda dengan benar</span>
-        <input type="text" class="input-form" placeholder="Nama Depan">
-        <input type="text" class="input-form" placeholder="Nama Belakang">
-        <input type="email" class="input-form" placeholder="namaanda@gmail.com">
-        <input type="password" class="input-form" placeholder="Password">
-        <button class="button-form button submit">SIGN UP</button>
-      </form>
-    </div>
-
-    <div class="container signin" id="signin">
-      <form action="#" method="POST" id="signin-form" class="form">
-        <h2>Selamat datang kembali,</h2>
-        <div class="icons-form">
-          <a href="https://www.facebook.com" class="icons-form"><i class="fab fa-facebook-f"></i></a>
-          <a href="https://www.instagram.com" class="icons-form"><i class="fab fa-instagram"></i></a>
-          <a href="https://www.linkedin.com" class="icons-form"><i class="fab fa-linkedin-in"></i></a>
-        </div>
-        <span>silahkan masukkan data anda dengan benar</span>
-        <input type="email" class="input-form" placeholder="namaanda@gmail.com">
-        <input type="password" class="input-form" placeholder="password">
-        <button class="button-form button submit">SIGN IN</button>
-        <a href="#" class="link-form">Lupa kata sandi?</a>
-      </form>
-    </div>
-    
-    <div class="switch" id="switch">
-      <div class="circle-switch"></div>
-      <div class="circle-switch t-circle-switch"></div>
-      <div class="switch-container" id="signin-switch">
-        <h2 class="switch-title title">Sudah memiliki akun ?</h2>
-        <p class="desc-switch desc">Silahkan masuk dengan akun anda, agar tetap terhubung dengan kami.</p>
-        <button class="switch-button button switch-btn">SIGN IN</button>
-      </div>
-
-      <div class="switch-container is-hidden" id="signup-switch">
-        <h2 class="switch-title title">Belum punya akun ?</h2>
-        <p class="desc-switch desc">Silahkan buat akun baru, agar tetap terhubung dengan kami.</p>
-        <button class="switch-button button switch-btn">SIGN UP</button>
-      </div>
-    </div>
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
   </div>
   <!-- end form login -->
 
